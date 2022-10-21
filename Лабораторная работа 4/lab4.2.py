4c38762e4f62c60f8cf2708f3b76f44b76dc0ce1
EvgeniyaBelovodchenko <115228595+EvgeniyaBelovodchenko@users.noreply.github.com>
2022-10-21 12:24:54 +0300

    Лабораторная работа 4

---
+++ Лабораторная работа 4/lab4.2.py
@@ -0,0 +1,19 @@
+'''
+Реализовать функцию delete, принимающую список и индекс, по которому надо удалить элемент.
+По умолчанию удается последний элемент из списка.
+Результатом вернуть измененный список.
+'''
+
+def delete(list_, index=None):
+    if index is None:
+        list_.pop()
+        return list_
+    else:
+        left_part = list_[:index]
+        right_part = list_[index + 1:]
+        new_list = left_part + right_part
+        return new_list
+
+print(delete([0, 0, 1, 2], index=0))  # [0, 1, 2]
+print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2, 3]
+print(delete([0, 1, 2, 3, 4, 4]))  # [0, 1, 2, 3, 4]
